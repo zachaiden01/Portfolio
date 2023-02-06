06a2cc4772dfff4f93c01b969dcfee13fe357487
zachaiden01 <johnanthony1104@gmail.com>
2023-02-06 13:49:00 +0800

    First commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("btn-back-to-top");
+  let progressValue = document.getElementById("progress-bar");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#343a40 ${scrollValue}%, #6c757d ${scrollValue}%)`;
+};
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('nav');
+burger.addEventListener('click', function() {
+  nav.classList.toggle('nav-open');
+});
